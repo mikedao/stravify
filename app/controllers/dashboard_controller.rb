e3dce99c2f7c27109089b7902e5bdc71766ac6c3
Michael Dao <michael.dao@gmail.com>
2015-03-19 10:41:42 -0600

    Added more test coverage

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,7 @@ class DashboardController < ApplicationController
   def index
     @bikes = sorted_bikes
     @clubs = session[:clubs]
-    @ftp = session[:ftp] || 0
+    @ftp = session[:ftp]
     @member_since = session[:member_since][0..3]
     @activities = Activity.all(current_user.token)
   end
--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,3 +1,17 @@
 require "rails_helper"
 RSpec.describe DashboardController, type: :controller do
+  it "hits dashboard controller" do
+    session[:bikes] = [{name: "Bike", distance: 1000}]
+    session[:clubs] = []
+    session[:ftp] = 200
+    session[:member_since] = "2011"
+    user = User.create(first_name: "Mike", token: ENV['STRAVA_PERSONAL'])
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+
+    expect(response.status).to eq(200)
+
+
+    get :index
+  end
 end
