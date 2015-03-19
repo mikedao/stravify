7a1cdccb1aa6370703089cfaae78b29d79044ed9
Michael Dao <michael.dao@gmail.com>
2015-03-19 10:12:03 -0600

    Added further controller testing

--- spec/controllers/api/v1/user_controller_spec.rb
+++ spec/controllers/api/v1/user_controller_spec.rb
@@ -9,6 +9,11 @@ RSpec.describe Api::V1::UserController, type: :controller do
                 distance_ridden: 400)
     get :show, format: :json, id: 100
 
+    result = JSON.parse(response.body)
+
     expect(response.status).to eq(200)
+    expect(result["ftp"]).to eq(200)
+    expect(result["time"]).to eq(300)
+    expect(result["distance"]).to eq(400)
   end
 end
--- spec/controllers/data_controller_spec.rb
+++ spec/controllers/data_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe DataController, :type => :controller do
+  it "provides bike data" do
+    session[:bikes] =[ { "name" => "Bike", "distance" => "100000" }]
 
+    get :bikedata, format: :json
+    result = JSON.parse(response.body)
+
+    expect(result.first).to eq(100)
+  end
 end
